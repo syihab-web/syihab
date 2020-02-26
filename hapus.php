aa45cfb4ef7781fa1e90a93709a26ee18d189209
Muhamad Syihabudin <syihabudin234@gmail.com>
2020-02-26 21:02:53 +0700

    Revisi tambah dan hapus

--- hapus.php
+++ hapus.php
@@ -1,8 +1,37 @@
 <?php 
 $nps = ["Enuma Elish","Vasavi Shakti","Rhongomyniad","Gae Bolg"];
-
-echo "Daftar noble phantasm Lancer Class";
-for($i = 0; $i<count($nps); $i++){
-    echo $nps[$i]."<br>";
-}
+$hero = ["Enkidu","Karna","Arthur Pendragon", "Cu Chulain"];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar servant Lancer with Noble Phantasm</title>
+</head>
+<body>
+    <h1 align="center">Daftar Noble Phantasm Lancer Class</h1>
+    <table border="1" align="center" width="40%">
+        <tr>
+            <th>Hero</th>
+            <th>Noble Phantasm</th>
+        </tr>
+        <tr>
+            <td><?php echo $hero[0];?></td>
+            <td><?php echo $nps[0];?></td>
+        </tr>
+        <tr>
+            <td><?php echo $hero[1];?></td>
+            <td><?php echo $nps[1];?></td>
+        </tr>
+        <tr>
+            <td><?php echo $hero[2];?></td>
+            <td><?php echo $nps[2];?></td>
+        </tr>
+        <tr>
+            <td><?php echo $hero[3];?></td>
+            <td><?php echo $nps[3];?></td>
+        </tr>
+    </table>
+</body>
+</html>
--- tambah.php
+++ tambah.php
@@ -1,8 +1,39 @@
 <?php 
+$hero = ["Gilgamesh","Emiya","Gilgamesh",'Arjuna'];
 $np = ["Enuma Elish","Unlimited Blade Works","Gate of Babylon","Pashupata"];
 
-echo "Daftar noble phantasm Archer Class";
-for($i = 0; $i<count($np); $i++){
-    echo $np[$i]."<br>";
-}
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar servant Archer with Noble Phantasm</title>
+</head>
+<body>
+    <h1 align="center">Daftar Noble Phantasm Archer Class</h1>
+    <table border="1" align="center" width="40%">
+        <tr>
+            <th>Hero</th>
+            <th>Noble Phantasm</th>
+        </tr>
+        <tr>
+            <td><?php echo $hero[0];?></td>
+            <td><?php echo $np[0];?></td>
+        </tr>
+        <tr>
+            <td><?php echo $hero[1];?></td>
+            <td><?php echo $np[1];?></td>
+        </tr>
+        <tr>
+            <td><?php echo $hero[2];?></td>
+            <td><?php echo $np[2];?></td>
+        </tr>
+        <tr>
+            <td><?php echo $hero[3];?></td>
+            <td><?php echo $np[3];?></td>
+        </tr>
+    </table>
+</body>
+</html>
