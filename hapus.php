a2c733e2cdcd4c8e0df8619cbd1f3645fb649d2f
Muhamad Syihabudin <syihabudin234@gmail.com>
2020-02-26 20:44:44 +0700

    Commit file tambah dan hapus

---
+++ hapus.php
@@ -0,0 +1,8 @@
+<?php 
+$nps = ["Enuma Elish","Vasavi Shakti","Rhongomyniad","Gae Bolg"];
+
+echo "Daftar noble phantasm Lancer Class";
+for($i = 0; $i<count($nps); $i++){
+    echo $nps[$i]."<br>";
+}
+?>
---
+++ tambah.php
@@ -0,0 +1,8 @@
+<?php 
+$np = ["Enuma Elish","Unlimited Blade Works","Gate of Babylon","Pashupata"];
+
+echo "Daftar noble phantasm Archer Class";
+for($i = 0; $i<count($np); $i++){
+    echo $np[$i]."<br>";
+}
+?>
