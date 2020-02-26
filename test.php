4911205b5caa135aaa38bce4227b77c9e0d93f3a
Muhamad Syihabudin <syihabudin234@gmail.com>
2020-02-26 10:58:42 +0700

    Revisi pertama test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+    $test = "Halo, kita sedang belajar Git ";
+    $language = ["Java","PHP","Python"];
+
+    echo $test."<br>";
+    echo "contoh bahasa Pemrograman";
+    for($i=0; $i < count($language); $i++){
+    echo $language[$i]."<br>";
+}
+?>
